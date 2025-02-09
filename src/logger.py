4ca7ddef1cbc5487904775fb09dbc57a83ed6336
Apoorv <apoorvpatidar.ap24@gmail.com>
2025-02-09 22:33:54 +0530

    progress

---
+++ src/logger.py
@@ -0,0 +1,9 @@
+## Simple purpose of this file is to keet the log of all the things that are happening in the application.
+
+import logging
+import os 
+from datetime import datetime 
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(logs_path, exist_ok=True)
